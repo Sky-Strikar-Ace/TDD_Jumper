57cbfd831a3cd232f4f2556984be387d7c2ba481
Alice1412 <sinon.tf.ayaya@gmail.com>
2022-08-23 16:30:37 +0900

    スケーリングの調整と障害物の追加

--- TDD_Jumper/MainPage.xaml.cs
+++ TDD_Jumper/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,7 +27,10 @@ namespace TDD_Jumper
         BitmapImage[] images = new BitmapImage[8];
         DispatcherTimer timer;
         int counter = 0;
-
+        int speed = 0;
+        int boxX = 600;
+        int boxY = 300;
+        int stickY = 200;
 
         public MainPage()
         {
@@ -42,12 +46,39 @@ namespace TDD_Jumper
             timer.Tick += Tick;
             timer.Start();
 
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+
+        }
+
+        private void CoreWindow_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            if (args.VirtualKey == Windows.System.VirtualKey.Space && stickY == 200)
+            {
+                speed = -15;
+            }
         }
 
         private void Tick(object sender, object e)
         {
             counter = (counter + 1) % 8;
             Stickman.Source = images[counter];
+
+            boxX = (boxX > -100) ? boxX - 10 : 600;
+            
+            Box.SetValue(Canvas.LeftProperty, boxX);
+            Box.SetValue(Canvas.TopProperty, boxY);
+
+
+            speed += 1;
+            stickY += speed;
+            if (stickY > 200)
+            {
+                stickY = 200;
+                speed = 0;
+            }
+
+            Stickman.SetValue(Canvas.TopProperty,stickY);
+
         }
     }
 }
